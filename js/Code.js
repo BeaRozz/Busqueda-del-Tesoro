9fc4de9c356100c0d2474617fd33b6ed2b14bb9e
BeaRozz <88104482+BeaRozz@users.noreply.github.com>
2021-12-16 12:23:19 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCION";
+const accessCode3 = "BASEDEDATOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Pista: Siempre cambia ¡NO es constante!", 100,70)
+    text("C U N I F O N", 700, 150)
+    text("Pista: Realiza una tarea en específico", 700, 170)
+    text("E D A T E D O S B A S", 100, 250)
+    text("Pista: Almacena la información (Escríbelo junto)", 100, 270)
+
+    
+
+}
